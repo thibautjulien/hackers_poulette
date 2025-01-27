3fc430c790e77b68978beceb24bf0aaa711236d0
Clean-Pick <benjamin.will98@gmail.com>
2025-01-27 16:47:44 +0100

    PHP init.
    Help.

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,11 @@
 <?php
-    try {
-        $bdd = new PDO('mysql:host=localhost;dbname=hackers_poulette;charset=utf8', 'root', '');
-        // Affichage des erreurs pour afficher les erreurs SQL
-        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (Exception $e) {
-        die('Erreur : ' . $e->getMessage());
-    }
+
+
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=hackers_poulette;charset=utf8', 'root', '');
+    // Affichage des erreurs pour afficher les erreurs SQL
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
 ?>
--- index.php
+++ index.php
@@ -1,49 +1,59 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Hackers Poulette</title>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hackers Poulette</title>
 
-        <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
 
-        <!-- Bootstrap -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
-    </head>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"
+            defer></script>
+</head>
 
-    <body>
-        <section id="form">
-            <form action="">
-                <div class="card w-50 p-5">
-                    <div class="d-flex flex-row">
-                        <!-- Input NAME -->
-                        <input type="text" class="form-control me-3" id="name" name="name" placeholder="Your name" minlength="2" maxlength="255" required>
+<body>
+<section id="form">
+    <form action="">
+        <div class="card w-50 p-5">
+            <div class="d-flex flex-row">
+                <!-- Input NAME -->
+                <input type="text" class="form-control me-3" id="name" name="name" placeholder="Your name" minlength="2"
+                       maxlength="255" required>
 
-                        <!-- Input FIRSTNAME -->
-                        <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Your firstname" minlength="2" maxlength="255" required>
-                    </div>
+                <!-- Input FIRSTNAME -->
+                <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Your firstname"
+                       minlength="2" maxlength="255" required>
+            </div>
 
-                    <div class="my-3">
-                        <!-- Input EMAIL -->
-                        <input type="email" class="form-control" id="email" name="email" placeholder="Your email" required>
-                    </div>
-                    
+            <div class="my-3">
+                <!-- Input EMAIL -->
+                <input type="email" class="form-control" id="email" name="email" placeholder="Your email" required>
+            </div>
 
-                    <!-- Input FILE -->
-                    <input type="file" class="form-control my-3" id="file" name="file" placeholder="Your file" required>
 
-                    <!-- Input DESCRIPTION -->
-                    <textarea type="text" class="form-control my-3" id="text" name="text" placeholder="Your description" minlength="2" maxlength="1000" required></textarea>
+            <!-- Input FILE -->
+            <input type="file" class="form-control my-3" id="file" name="file" placeholder="Your file" required>
 
-                    <button type="submit" class="my-3">Submit</button>
-                </div>
-                    
-            </form>
-        </section>
+            <!-- Input DESCRIPTION -->
+            <textarea type="text" class="form-control my-3" id="text" name="text" placeholder="Your description"
+                      minlength="2" maxlength="1000" required></textarea>
 
-        <?php
-            require_once 'includes/db.php';
-        ?>
-    </body>
+            <button type="submit" class="my-3">Submit</button>
+        </div>
+
+    </form>
+</section>
+
+<?php
+require_once 'includes/db.php';
+?>
+</body>
 </html>
