61dc75120ce6fa4c4df64b9407795cd26e293781
Howlnes/Thibaut <thbtjulien21@gmail.com>
2025-01-28 09:10:21 +0100

    mise en place dotenv

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,15 @@
 <?php
+    require_once __DIR__ . '../../vendor/autoload.php';
+    use Dotenv\Dotenv;
+
+    // Initialiser Dotenv et charger les variables d'environnement
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+    
     try {
-        $bdd = new PDO('mysql:host=localhost;dbname=hackers_poulette;charset=utf8', 'root', '');
+        $password = $_ENV['PASSWORD_DB'] ?? '';
+
+        $bdd = new PDO('mysql:host=localhost;dbname=hackers_poulette;charset=utf8', 'root', $password);
         // Affichage des erreurs pour afficher les erreurs SQL
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (Exception $e) {
--- index.php
+++ index.php
@@ -1,49 +1,56 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Hackers Poulette</title>
 
-        <link rel="stylesheet" href="assets/css/styles.css">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hackers Poulette</title>
 
-        <!-- Bootstrap -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
-    </head>
+    <link rel="stylesheet" href="assets/css/styles.css">
 
-    <body>
-        <section id="form">
-            <form action="">
-                <div class="card w-50 p-5">
-                    <div class="d-flex flex-row">
-                        <!-- Input NAME -->
-                        <input type="text" class="form-control me-3" id="name" name="name" placeholder="Your name" minlength="2" maxlength="255" required>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"
+        defer></script>
+</head>
 
-                        <!-- Input FIRSTNAME -->
-                        <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Your firstname" minlength="2" maxlength="255" required>
-                    </div>
+<body>
+    <section id="form">
+        <form action="">
+            <div class="card w-50 p-5">
+                <div class="d-flex flex-row my-3">
+                    <!-- Input NAME -->
+                    <input type="text" class="form-control me-3" id="name" name="name" placeholder="Your name"
+                        minlength="2" maxlength="255" required>
 
-                    <div class="my-3">
-                        <!-- Input EMAIL -->
-                        <input type="email" class="form-control" id="email" name="email" placeholder="Your email" required>
-                    </div>
-                    
-
-                    <!-- Input FILE -->
-                    <input type="file" class="form-control my-3" id="file" name="file" placeholder="Your file" required>
-
-                    <!-- Input DESCRIPTION -->
-                    <textarea type="text" class="form-control my-3" id="text" name="text" placeholder="Your description" minlength="2" maxlength="1000" required></textarea>
-
-                    <button type="submit" class="my-3">Submit</button>
+                    <!-- Input FIRSTNAME -->
+                    <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Your firstname"
+                        minlength="2" maxlength="255" required>
                 </div>
-                    
-            </form>
-        </section>
 
-        <?php
+                <div class="my-3">
+                    <!-- Input EMAIL -->
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Your email" required>
+                </div>
+
+                <!-- Input FILE -->
+                <input type="file" class="form-control my-3" id="file" name="file" placeholder="Your file">
+
+                <!-- Input DESCRIPTION -->
+                <textarea type="text" class="form-control my-3" id="text" name="text" placeholder="Your description"
+                    minlength="2" maxlength="1000" required></textarea>
+
+                <button type="submit" class="my-3">Submit</button>
+            </div>
+
+        </form>
+    </section>
+
+    <?php
             require_once 'includes/db.php';
         ?>
-    </body>
+</body>
+
 </html>
